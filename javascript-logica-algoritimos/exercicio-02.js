cb0f0cee68239e2d66ac70369655ca3e95a195a9
joaocla <jc.claudio.bento@gmail.com>
2021-07-30 18:24:20 -0300

    Adiciona exercicio2

---
+++ javascript-logica-algoritimos/exercicio-02.js
@@ -0,0 +1,19 @@
+// Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+// n = 5
+
+// *
+// **
+// ***
+// ****
+// *****
+
+const n = 5;
+const joker = '*';
+let contador = 1;
+
+for(i = 1; i <= n; i ++){
+    while(contador <= n){
+        console.log(joker.repeat(contador));
+        contador ++;
+    }
+}
